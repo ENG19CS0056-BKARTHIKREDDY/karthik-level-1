8965dfadb55a6e9dc1762d989181ea964fdee6a2
ENG19CS0056-BKARTHIKREDDY <76103778+ENG19CS0056-BKARTHIKREDDY@users.noreply.github.com>
2021-03-31 21:36:49 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,28 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+#include<stdio.h>
+float input(char a)
+{
+    float n;
+    printf("enter the values of tromboloid h,d,b: ");
+    scanf("%f",&n);
+    return n;
+}
+float volume(float h,float d,float b)
+{
+    float volume=(((h*d*b)+(d/b))/3);
+    return volume;
+}
+void output(float v)
+{
+    printf("the volume of tromboloid is %f",v);
+}
+int main()
+{
+    float h,d,b,v;
+    h=input(h);
+    d=input(d);
+    b=input(b);
+    v=volume(h,d,b);
+    output(v);
+    return 0;
+}
