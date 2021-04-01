2ec3259b3944c3357b81e428e8729e53c3d2cd6f
ENG19CS0056-BKARTHIKREDDY <76103778+ENG19CS0056-BKARTHIKREDDY@users.noreply.github.com>
2021-04-01 13:40:26 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,41 +1,25 @@
 //WAP to find the distance between two point using 4 functions.
 #include<stdio.h>
 #include<math.h>
-
-void inp(float *x1,float *y1,float *x2,float *y2)
+float input()
 {
-	printf("enter the value of x1:");
-	scanf("%f",x1);
-   	printf("enter the value of y1:");
-	scanf("%f",y1);
-    printf("enter the value of x2:");
-	scanf("%f",x2);
-	printf("enter the value of y2:");
-	scanf("%f",y2);
-
+    float x1,y1,x2,y2;
+    printf("enter the coordinates of points:");
+    scanf("%f,%f,%f,%f",&x1,&y1,&x2,&y2);
 }
-
-float Distance(float x1, float y1,  float x2, float y2)
+float distance(float x1,float y1,float x2,float y2)
 {
-	float d;
-	d=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-	return d;
+    float d;
+    d=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    return d;
 }
-
-
-void out(float x1,float y1,float x2,float y2,float sq)
-{
-	printf("The Distance between (%.3f ,%.3f) ,(%.3f ,%.3f) Is %.3f .",x1,y1,x2,y2,sq);
-}
-
-
-
 int main()
 {
-	float x1,x2,y1,y2,square_root;
-    	inp(&x1,&y1,&x2,&y2);
-	square_root=Distance(x1,x2,y1,y2);
-	out(x1,y1,x2,y2,square_root);
-	return 0;
+    float x1,y1,x2,y2,d;
+    x1=input();
+    y1=input();
+    x2=input();
+    y2=input();
+    d=distance(x1,y1,x2,y2);
+    return 0;
 }
-
